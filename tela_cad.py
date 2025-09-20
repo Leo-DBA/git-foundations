847ea9aae0542f78408972235a500e9106f0b684
leonardo-mdsilva <leeodba@gmail.com>
2025-09-20 20:13:16 -0300

    add o menu em uma def

--- tela_cad.py
+++ tela_cad.py
@@ -1,12 +1,17 @@
-print("""
-      
-      
-        SISTEMA VALIDATION!!
-      
 
-      ESCOLHA A OPÇÃO ABAIXO:
-      1- Login
-      2 - Criar Úsuario
-      3 - Sair
-      
-      """)
+
+def ini():
+
+
+  print("""
+        
+        
+          SISTEMA VALIDATION!!
+        
+
+        ESCOLHA A OPÇÃO ABAIXO:
+        1- Login
+        2 - Criar Úsuario
+        3 - Sair
+        
+        """)
